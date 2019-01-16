14aee688e67ab80273c077dd3eff7ea4d61b2ce9
tanxingwen <tanxingwen@hifive.ai>
2019-01-16 15:08:21 +0800

    update config

--- app/js/router/index.js
+++ app/js/router/index.js
@@ -8,12 +8,37 @@ import '../../css/reset.scss'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: Home,
+      meta: { requiresAuth: true }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // const uid = localStorage.getItem('uid') || ''
+    // ios 9.3.2 不支持localStorage
+    const uid = AGM_USER.uid
+    if (uid) {
+      next()
+    } else {
+      next({
+        path: '/',
+        name: 'home'
+      })
+    }
+  } else {
+    next()
+  }
+})
+export default router
--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin') //压缩js，并配置sourceMap
 
+function assetsPath(_path) {
+  return path.posix.join('static', _path)
+}
+
 module.exports = env => {
   if (!env) {
     env = {}
@@ -22,7 +26,7 @@ module.exports = env => {
     plugins.push(
       new webpack.DefinePlugin({
         'process.env': {
-          NODE_ENV: 'production'
+          NODE_ENV: JSON.stringify('production')
         }
       }),
       new ExtractTextPlugin('style.css'),
@@ -55,34 +59,61 @@ module.exports = env => {
               ? {
                   css: ExtractTextPlugin.extract({
                     use:
-                      'css-loader?minimize!px2rem-loader?remUnit=75&remPrecision=8',
+                      'css-loader?minimize!px2rem-loader?remUnit=45&remPrecision=8',
                     fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
                   }),
                   scss: ExtractTextPlugin.extract({
                     use:
-                      'css-loader?minimize!px2rem-loader?remUnit=75&remPrecision=8!sass-loader',
+                      'css-loader?minimize!px2rem-loader?remUnit=45&remPrecision=8!sass-loader',
                     fallback: 'vue-style-loader'
                   })
                 }
               : {
                   css:
-                    'vue-style-loader!css-loader!px2rem-loader?remUnit=75&remPrecision=8',
+                    'vue-style-loader!css-loader!px2rem-loader?remUnit=45&remPrecision=8',
                   scss:
-                    'vue-style-loader!css-loader!px2rem-loader?remUnit=75&remPrecision=8!sass-loader'
-                },
-            formatter: require('eslint-friendly-formatter'),
-            emitWarning: true
+                    'vue-style-loader!css-loader!px2rem-loader?remUnit=45&remPrecision=8!sass-loader'
+                }
           }
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
         },
         {
           test: /\.scss$/,
           loader: 'style-loader!css-loader!sass-loader'
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: assetsPath('img/[name].[hash:7].[ext]')
+          }
+        },
+        {
+          test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: assetsPath('media/[name].[hash:7].[ext]')
+          }
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: assetsPath('fonts/[name].[hash:7].[ext]')
+          }
         }
       ]
     },
     devServer: {
       contentBase: path.join(__dirname, 'dist'),
       compress: true,
+      host: '0.0.0.0',
       port: 9090,
       overlay: true
     },
@@ -95,7 +126,7 @@ module.exports = env => {
       }
     },
     output: {
-      filename: '[name].min.js',
+      filename: '[name]-[hash:5].js',
       path: path.resolve(__dirname, 'dist')
     }
   }
